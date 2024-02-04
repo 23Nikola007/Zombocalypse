da61540974e76e455b64dd65811e9bb737f77e85
VeljkoS2 <lukaveljkolukaveljko@gmail.com>
2024-02-04 19:38:36 +0100

    moving zombies

--- Zombocalypse/Assets/Enemies/MoveTowardsPlayer.cs
+++ Zombocalypse/Assets/Enemies/MoveTowardsPlayer.cs
@@ -1,17 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class MOv : MonoBehaviour
 {
-    public GameObject Target;
     public GameObject Follower;
     public float speed;
+    public Vector3 PointPos;
+    private bool gotToPoint;
     // Update is called once per frame
-    void Update()
+    bool CheckIfPointPos(Vector3 Pos, Vector3 CurrentPos)
     {
-        Vector3 targetPos = Target.transform.position;
+        if((Pos - CurrentPos).magnitude <= 0.1 && !gotToPoint)
+        {
+            gotToPoint = true;
+            return gotToPoint;
+        }
+        return gotToPoint;
+    }
+    void FixedUpdate()
+    {
+        
+        PointPos.y = Follower.transform.position.y;
         Vector3 FollowerPos = Follower.transform.position;
-        Follower.transform.position = Vector3.MoveTowards(FollowerPos, targetPos, Time.deltaTime * speed);
+        if (CheckIfPointPos(PointPos, FollowerPos))
+        {
+            GameObject Target = GameObject.FindWithTag("Player");
+            Vector3 targetPos = Target.transform.position;
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.velocity = speed * (new Vector3(targetPos.x, 0, targetPos.z) - new Vector3(FollowerPos.x, 0, FollowerPos.z)).normalized;
+            gameObject.transform.LookAt(new Vector3(targetPos.x, gameObject.transform.position.y, targetPos.z));
+            rb.angularVelocity = new Vector3(0, 0, 0);
+        }
+        else 
+        {
+            //Follower.transform.LookAt(PointPos);
+            //Follower.transform.position = Vector3.MoveTowards(FollowerPos, PointPos, Time.deltaTime * speed);
+            //Follower.transform.Rotate(-Follower.transform.rotation.eulerAngles.x, 0, -Follower.transform.rotation.eulerAngles.z);
+            Rigidbody rb = gameObject.GetComponent<Rigidbody>();
+            rb.velocity = speed * (PointPos-gameObject.transform.position).normalized;
+            gameObject.transform.LookAt(PointPos);
+            rb.angularVelocity = new Vector3(0, 0, 0);
+        }
     }
 }
